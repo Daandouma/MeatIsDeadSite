8cfc1285358e32882a04aff3b322de7b324d948c
Daandouma <40421292+Daandouma@users.noreply.github.com>
2025-11-24 16:14:34 +0100

    Create mastodon.js

---
+++ mastodon.js
@@ -0,0 +1,25 @@
+const mastodonFeed = document.getElementById("mastodon-feed");
+
+async function loadMastodon() {
+  try {
+    const res = await fetch("https://mastodon.social/api/v1/accounts/lookup?acct=Meatisdead");
+    const account = await res.json();
+
+    const postsRes = await fetch(
+      `https://mastodon.social/api/v1/accounts/${account.id}/statuses?exclude_replies=true&exclude_reblogs=true`
+    );
+    const posts = await postsRes.json();
+
+    mastodonFeed.innerHTML = posts.slice(0, 5).map(p => `
+      <div class="toot metal-box">
+        ${p.content}
+        <br><a href="${p.url}" target="_blank">Bekijk post</a>
+      </div>
+    `).join("");
+
+  } catch (err) {
+    mastodonFeed.innerHTML = "Kon posts niet laden.";
+  }
+}
+
+if (mastodonFeed) loadMastodon();
